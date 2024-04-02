a573d2c1587b7e2cf3927b413d5d8260335948eb
Bart <98636497+madejbart@users.noreply.github.com>
2024-04-02 15:54:27 +0200

    Update models.py

--- testapp/models.py
+++ testapp/models.py
@@ -55,56 +55,6 @@ class User(models.Model):
     def __str__(self):
         return f"{self.first_name} ({self.email})"
 
-
-class Value(models.Model):
-    date_created = models.DateTimeField(blank=True, null=True)
-    var = models.ForeignKey('Var', models.DO_NOTHING, blank=True, null=True)
-    value = models.TextField(blank=True, null=True)  # This field type is a guess.
-
-    class Meta:
-        db_table = 'value'
-
-class Var(models.Model):
-    value_name = models.CharField(blank=True, null=True, max_length=255)
-    date = models.DateTimeField(blank=True, null=True)
-    user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
-    device = models.ForeignKey(Device, models.DO_NOTHING, blank=True, null=True)
-    date_updated = models.DateTimeField(blank=True, null=False, default=datetime.now())
-    frequency = models.IntegerField(blank=True, null=True)
-    enabled = models.BooleanField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'var'
-
-class Devmtcpserver(models.Model):
-    device_name = models.CharField(blank=True, null=True, max_length=255)
-    date = models.DateTimeField(blank=True, null=True)
-    device_model = models.CharField(blank=True, null=True, max_length=255)
-    device_ip = models.CharField(blank=True, null=True, max_length=255)
-    user = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'deviceserver'
-
-class Mtcpclient(models.Model):
-    name = models.CharField(blank=True, null=True, max_length=255)
-    date = models.DateTimeField(blank=True, null=True)
-    server = models.ForeignKey(Devmtcpserver, models.DO_NOTHING, blank=True, null=True)
-    date_updated = models.DateTimeField(blank=True, null=False, default=datetime.now())
-    regfirst = models.IntegerField(blank=True, null=True)
-    regamount = models.IntegerField(blank=True, null=True)
-    frequency = models.IntegerField(blank=True, null=True)
-    enabled = models.BooleanField(blank=True, null=True)
-
-class Registers(models.Model):
-    name = models.CharField(blank=True, null=True, max_length=255)
-    date = models.DateTimeField(blank=True, null=True)
-    client = models.ForeignKey(Mtcpclient, models.DO_NOTHING, blank=True, null=True)
-    date_updated = models.DateTimeField(blank=True, null=False, default=datetime.now())
-    regnumber = models.IntegerField(blank=True, null=True)
-
-
 class ModbusDevice(models.Model):
     name = models.CharField(max_length=100)
     ip = models.CharField(max_length=20)
