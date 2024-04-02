5e1670274c2b680c44d193582d94094605761c0a
Bart <98636497+madejbart@users.noreply.github.com>
2024-04-02 15:57:26 +0200

    Update admin.py

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Device, Var, Value, Note, Devmtcpserver, Mtcpclient, Registers
+from .models import User, Note
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Device)
-admin.site.register(Var)
-admin.site.register(Value)
 admin.site.register(Note)
-admin.site.register(Devmtcpserver)
-admin.site.register(Mtcpclient)
-admin.site.register(Registers)
